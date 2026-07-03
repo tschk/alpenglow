a388b6e9815562003b8d2a4e8b80039e42241858
Max Carter 祁明思 <undivisible@vk.com>
2026-07-03 13:34:58 +1000

    Document Orange Pi 3B SD MMC device

--- system/backends/rk3566/boot-orangepi-3b.cmd
+++ system/backends/rk3566/boot-orangepi-3b.cmd
@@ -1,7 +1,7 @@
 # Alpenglow RK3566 U-Boot boot script for Orange Pi 3B
 # Console: UART2 at 1500000 baud (RK3566 typical)
 setenv bootargs quiet console=ttyS2,1500000 rdinit=/init
-load mmc 0:1 ${kernel_addr_r} /vmlinuz
-load mmc 0:1 ${ramdisk_addr_r} /initramfs.cpio.zst
-load mmc 0:1 ${fdt_addr_r} /rk3566-orangepi-3b.dtb
+load mmc 1:1 ${kernel_addr_r} /vmlinuz
+load mmc 1:1 ${ramdisk_addr_r} /initramfs.cpio.zst
+load mmc 1:1 ${fdt_addr_r} /rk3566-orangepi-3b.dtb
 booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
