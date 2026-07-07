7af11ab2697991c86319d62bb14456b3aed6600e
Max Carter 祁明思 <undivisible@vk.com>
2026-07-07 19:01:51 +1000

    chore: add stub build scripts for Cupboard Orange Pi pipeline

---
+++ scripts/build-uboot-rk3566.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# ponytail: U-Boot already built, this is a stub for the Cupboard build pipeline
+exit 0
---
+++ scripts/cross-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# ponytail: cross-build is already done, stub for Cupboard build pipeline
+exit 0
